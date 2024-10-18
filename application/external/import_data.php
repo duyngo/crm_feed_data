a0cc94c57e44cf77af7d31f790b28fcc3f1c7db5
DuyNgo <duy.ngo@freighttrackertms.com>
2024-10-18 23:26:37 +0700

    downloadLargeFile

--- application/external/import_data.php
+++ application/external/import_data.php
@@ -3,16 +3,14 @@
 @ini_set('max_execution_time', 0);
 //echo 'Working.... ';
 
-function get_download_save_file($file_url, $file_name){
-     // Saves the file in the current folder
-    $saveTo = __DIR__ . "/$file_name";
+function get_download_save_file($file_url, $save_to){
 
     // Use file_get_contents() to read the file from the URL
     $fileContents = file_get_contents($file_url);
     if ($fileContents !== false) {
         // Save the file to the specified location using file_put_contents()
-        if (file_put_contents($saveTo, $fileContents)) {
-            echo "File downloaded and saved to: $saveTo";
+        if (file_put_contents($save_to, $fileContents)) {
+            echo "File downloaded and saved to: $save_to";
         } else {
             echo "Error saving the file.";
         }
@@ -33,13 +31,31 @@ function get_download_save_file($file_url, $file_name){
     }
 }
 
+function downloadLargeFile($url, $output_file_path) {
+    $fp = fopen($output_file_path, 'w+');
+    
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_FILE, $fp);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_BUFFERSIZE, 128 * 1024);  // Set buffer size (128KB)
+    curl_setopt($ch, CURLOPT_TIMEOUT, 600); // Timeout in seconds
+    
+    curl_exec($ch);
+    curl_close($ch);
+    fclose($fp);
+
+    echo "File downloaded and saved to: $output_file_path <br/>";
+}
+    
+
 function process_dicker_data(){
     $CI = & get_instance();
 
     $file_url = 'https://portal.dickerdata.com.au/Download?file=raqJl1tyq4ZEzYDHtVUsOuSl4zAncwx%2Fzg%2Bx0ubLlU%2FenFx3QTFg2jNviQgIJcThjqyfwPHYyhsoCvjDX8xtrx1WQ2i3huc%2B%2FvGHrRkVJXCT9rf08l5fbXhHHBXMAayhGWNlC62UBLZqA9qvGGDMpBRQ3Do7jEnK&fileName=DickerDataDataFeedCSV.csv&displaySaveAs=True';
-    $file_name = 'DickerData.csv';
+    $output_file_path = __DIR__ . "/DickerData.csv"; 
 
-    $data = get_download_save_file($file_url, $file_name);
+    //$data = get_download_save_file($file_url, $file_name);
+    downloadLargeFile($file_url, $output_file_path);
 
     $map_col_excel2db = [
         'StockCode' => 'stock_code',
@@ -58,26 +74,28 @@ function process_dicker_data(){
     ];
 
     //get first element of data
-    $header_excel = array_shift($data);
+  
     $bulk_param_data = array();
-    foreach($data as $index => $row){
-        // Skip empty rows
-        //array_filter If no callback is supplied, all empty entries of array will be removed
-        if (empty(array_filter($row))) {
-            continue;
-        }
-        $param_data = array();
-        foreach ($map_col_excel2db as $csvColumn => $dbColumn) {
-            // Find the index of the CSV column in the header
-            $csvIndex = array_search($csvColumn, $header_excel); 
-            // Get the corresponding CSV value, fallback to empty string if not found
-            $param_data[$dbColumn] = $row[$csvIndex] ?? ''; 
-        }
-
-        $bulk_param_data[] = $param_data;
-        if($index % 300 == 0){
-            $CI->db->insert_batch('crm_product_stock', $bulk_param_data);
-            $bulk_param_data = array();
+    if (($handle = fopen($output_file_path, "r")) !== FALSE) {
+        // Get the header row to determine CSV columns
+        $header_excel = fgetcsv($handle);
+        $bulk_param_data = array();
+        $row_count = 0;
+        while (($row_data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $row_count ++;
+            $param_data = array();
+            foreach ($map_col_excel2db as $csvColumn => $dbColumn) {
+                // Find the index of the CSV column in the header
+                $csvIndex = array_search($csvColumn, $header_excel); 
+                // Get the corresponding CSV value, fallback to empty string if not found
+                $param_data[$dbColumn] = $row_data[$csvIndex] ?? ''; 
+            }
+    
+            $bulk_param_data[] = $param_data;
+            if($row_count % 300 == 0){
+                $CI->db->insert_batch('crm_product_stock', $bulk_param_data);
+                $bulk_param_data = array();
+            }
         }
     }
     if(!empty($bulk_param_data)){
